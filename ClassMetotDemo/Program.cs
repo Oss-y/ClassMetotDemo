22c08f1dbbd5d18d56397797a6af342c4494c2c6
Osman Patkavak <osmanpatkavak1@gmail.com>
2023-07-29 14:52:30 +0300

    güncelleme

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -11,33 +11,39 @@ namespace ClassMetotDemo
             //MusteriManager sınıfı oluşturunuz.Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
 
             Musteri customer1 = new Musteri();
-            musteri1.Adi = "Jack";
-            musteri1.Soyadi = "Humbler";
-            musteri1.Id = 00234532;
-            musteri1.CepNo = 512-342-8998;
+            customer1.Adi = "Jack";
+            customer1.Soyadi = "Humbler";
+            customer1.Id = 00234532;
+            customer1.CepNo = 5123428998;
 
             Musteri customer2 = new Musteri();
-            musteri2.Adi = "Cash";
-            musteri2.Soyadi = "Jora";
-            musteri2.Id = 98764531;
-            musteri2.CepNo = 765-345-2634;
+            customer2.Adi = "Cash";
+            customer2.Soyadi = "Jora";
+            customer2.Id = 98764531;
+            customer2.CepNo = 7653452634;
 
             Musteri customer3 = new Musteri();
             customer3.Adi = "Jay";
             customer3.Soyadi = "Fat";
             customer3.Id = 65748356;
-            customer3.CepNo = 751 - 876 - 8976;
+            customer3.CepNo = 7518768976;
 
             Musteri customer4= new Musteri();
             customer4.Adi = "Kemal";
             customer4.Soyadi = "Kurt";
             customer4.Id = 76538909;
-            customer4CepNo = 456 - 987 - 9009;
+            customer4.CepNo = 4569879009;
 
-            Musteri[] customer
+            Musteri[] customers = {customer1, customer2, customer3,customer4 };
+
+            foreach (Musteri customer in customers)
+            {
+                Console.WriteLine(customer.Adi + "  " + customer.Soyadi);
+                Console.WriteLine(customer.CepNo);
+            }
 
 
-                
+
         }
     }
 }
