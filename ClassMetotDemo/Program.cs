b2e38eceb4970ac9053f79da4ccd1574fdc61884
Osman Patkavak <osmanpatkavak1@gmail.com>
2023-07-29 14:46:20 +0300

    yenileme

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -5,8 +5,39 @@ namespace ClassMetotDemo
     class Program
     {
         static void Main(string[] args) 
-        { 
-            
+        {
+            //Bir bankada müşteri takibi yapmak istiyorsunuz.
+            //Musteri isimli bir Class oluşturunuz.Müşteriye istediğiniz özellikleri ekleyiniz. (Id, Ad, Soyad....)
+            //MusteriManager sınıfı oluşturunuz.Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
+
+            Musteri customer1 = new Musteri();
+            musteri1.Adi = "Jack";
+            musteri1.Soyadi = "Humbler";
+            musteri1.Id = 00234532;
+            musteri1.CepNo = 512-342-8998;
+
+            Musteri customer2 = new Musteri();
+            musteri2.Adi = "Cash";
+            musteri2.Soyadi = "Jora";
+            musteri2.Id = 98764531;
+            musteri2.CepNo = 765-345-2634;
+
+            Musteri customer3 = new Musteri();
+            customer3.Adi = "Jay";
+            customer3.Soyadi = "Fat";
+            customer3.Id = 65748356;
+            customer3.CepNo = 751 - 876 - 8976;
+
+            Musteri customer4= new Musteri();
+            customer4.Adi = "Kemal";
+            customer4.Soyadi = "Kurt";
+            customer4.Id = 76538909;
+            customer4CepNo = 456 - 987 - 9009;
+
+            Musteri[] customer
+
+
+                
         }
     }
 }
